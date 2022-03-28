7602ea2a07c3add918147747c150d02e112e29f4
Francesko Racaku <franceskoracaku@hotmail.com>
2022-03-28 14:23:38 -0400

    api test

--- src/main/java/moto/inventory/tables/motoInventory/MotorsController.java
+++ src/main/java/moto/inventory/tables/motoInventory/MotorsController.java
@@ -1,54 +1,54 @@
-//package moto.inventory.tables.motoInventory;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.*;
-//
-//import java.util.List;
-//import java.util.Optional;
-//
-//@RestController
-////@CrossOrigin(origins = "http://localhost:4200")
-//@RequestMapping(path = "motors")
-//
-//public class MotorsController {
-//
-//    private final MotorsService motorsService;
-//
-//    @Autowired
-//    public MotorsController(MotorsService motorsService) {
-//        this.motorsService = motorsService;
-//    }
-//
-//    @GetMapping
-//    public List<Motors> getMotors() {
-//        return motorsService.getMotors();
-//
-//    }
-//
-//    @GetMapping(path = "{id}")
-//    public Optional<Motors> getMotor(@PathVariable Integer id){
-//        return motorsService.getMotor(id);
-//    }
-//
-//    @PostMapping
-//    public void addMotor(@RequestBody Motors motors) {
-//        motorsService.addMotors(motors);
-//    }
-//
-//
-//    @DeleteMapping(path = "{motorId}")
-//    public void deleteMotor(@PathVariable("motorId") Integer motorId) {
-//        motorsService.deleteMotor(motorId);
-//    }
-//
-//    @PutMapping(path = "{motorId}")
-//    public void updateMotor(
-//            @PathVariable("motorId") Integer motorId,
-//            @RequestParam(required = false) String vin,
-//            @RequestParam(required = false) String make,
-//            @RequestParam(required = false) String type){
-//        motorsService.updateMotor(motorId, vin, make, type);
-//
-//    }
-//
-//}
+package moto.inventory.tables.motoInventory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+//@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping(path = "motors")
+
+public class MotorsController {
+
+    private final MotorsService motorsService;
+
+    @Autowired
+    public MotorsController(MotorsService motorsService) {
+        this.motorsService = motorsService;
+    }
+
+    @GetMapping
+    public List<Motors> getMotors() {
+        return motorsService.getMotors();
+
+    }
+
+    @GetMapping(path = "{id}")
+    public Optional<Motors> getMotor(@PathVariable Integer id){
+        return motorsService.getMotor(id);
+    }
+
+    @PostMapping
+    public void addMotor(@RequestBody Motors motors) {
+        motorsService.addMotors(motors);
+    }
+
+
+    @DeleteMapping(path = "{motorId}")
+    public void deleteMotor(@PathVariable("motorId") Integer motorId) {
+        motorsService.deleteMotor(motorId);
+    }
+
+    @PutMapping(path = "{motorId}")
+    public void updateMotor(
+            @PathVariable("motorId") Integer motorId,
+            @RequestParam(required = false) String vin,
+            @RequestParam(required = false) String make,
+            @RequestParam(required = false) String type){
+        motorsService.updateMotor(motorId, vin, make, type);
+
+    }
+
+}
--- src/main/java/moto/inventory/tables/motoInventory/MotorsService.java
+++ src/main/java/moto/inventory/tables/motoInventory/MotorsService.java
@@ -1,82 +1,82 @@
-//package moto.inventory.tables.motoInventory;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Service;
-//
-//import javax.transaction.Transactional;
-//import java.util.List;
-//import java.util.Objects;
-//import java.util.Optional;
-//
-//
-//@Service
-//
-//public class MotorsService {
-//
-//    private final MotorsRepository motorsRepository;
-//
-//    @Autowired
-//
-//    public MotorsService(MotorsRepository motorsRepository) {
-//        this.motorsRepository = motorsRepository;
-//    }
-//
-//    public List<Motors> getMotors() {
-//        return motorsRepository.findAll();
-//    }
-//
-//    public Optional<Motors> getMotor(Integer id){
-//        return motorsRepository.findById(id);
-//
-//    }
-//
-//    public void addMotors(Motors motors){
-//        Optional<Motors> motorsByMake = motorsRepository
-//                .findMotorsByVin(motors.getVin());
-//        if(motorsByMake.isPresent()){
-//            throw new IllegalStateException("VIN Taken");
-//        }
-//        motorsRepository.save(motors);
-//    }
-//
-//    public void deleteMotor(Integer motorId) {
-//        boolean exists = motorsRepository.existsById(motorId);
-//        if (!exists){
-//            throw new IllegalStateException(
-//                    "Motor with id "+ motorId + " does not exists!!");
-//        }
-//        motorsRepository.deleteById(motorId);
-//    }
-//
-//    @Transactional
-//    public void updateMotor(Integer motorId, String vin, String make, String type) {
-//        Motors motors = motorsRepository.findById(motorId)
-//                .orElseThrow(() -> new IllegalStateException(
-//                        "motor with id " + motorId + " does no exist!!")
-//                );
-//
-//
-//        if (vin != null &&
-//                vin.length() > 0 &&
-//                !Objects.equals(motors.getVin(), vin)) {
-//            Optional<Motors> motorsOptional = motorsRepository
-//                    .findMotorsByVin(vin);
-//            if (motorsOptional.isPresent()) {
-//                throw new IllegalStateException("VIN Taken");
-//            }
-//            motors.setVin(vin);
-//        }
-//
-//        if (make != null &&
-//                make.length() > 0 &&
-//                !Objects.equals(motors.getMake(), make)) {
-//            motors.setMake(make);
-//        }
-//
-//        if (type != null &&
-//                type.length() > 0 &&
-//                !Objects.equals(motors.getType(), type)) {
-//            motors.setType(type);
-//        }
-//    }
-//}
+package moto.inventory.tables.motoInventory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+
+@Service
+
+public class MotorsService {
+
+    private final MotorsRepository motorsRepository;
+
+    @Autowired
+
+    public MotorsService(MotorsRepository motorsRepository) {
+        this.motorsRepository = motorsRepository;
+    }
+
+    public List<Motors> getMotors() {
+        return motorsRepository.findAll();
+    }
+
+    public Optional<Motors> getMotor(Integer id){
+        return motorsRepository.findById(id);
+
+    }
+
+    public void addMotors(Motors motors){
+        Optional<Motors> motorsByMake = motorsRepository
+                .findMotorsByVin(motors.getVin());
+        if(motorsByMake.isPresent()){
+            throw new IllegalStateException("VIN Taken");
+        }
+        motorsRepository.save(motors);
+    }
+
+    public void deleteMotor(Integer motorId) {
+        boolean exists = motorsRepository.existsById(motorId);
+        if (!exists){
+            throw new IllegalStateException(
+                    "Motor with id "+ motorId + " does not exists!!");
+        }
+        motorsRepository.deleteById(motorId);
+    }
+
+    @Transactional
+    public void updateMotor(Integer motorId, String vin, String make, String type) {
+        Motors motors = motorsRepository.findById(motorId)
+                .orElseThrow(() -> new IllegalStateException(
+                        "motor with id " + motorId + " does no exist!!")
+                );
+
+
+        if (vin != null &&
+                vin.length() > 0 &&
+                !Objects.equals(motors.getVin(), vin)) {
+            Optional<Motors> motorsOptional = motorsRepository
+                    .findMotorsByVin(vin);
+            if (motorsOptional.isPresent()) {
+                throw new IllegalStateException("VIN Taken");
+            }
+            motors.setVin(vin);
+        }
+
+        if (make != null &&
+                make.length() > 0 &&
+                !Objects.equals(motors.getMake(), make)) {
+            motors.setMake(make);
+        }
+
+        if (type != null &&
+                type.length() > 0 &&
+                !Objects.equals(motors.getType(), type)) {
+            motors.setType(type);
+        }
+    }
+}
